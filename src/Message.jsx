5b0071e39e2e5a6b889fa355439f58043b855784
ymelki <67371192+ymelki@users.noreply.github.com>
2023-12-22 11:05:11 +0100

    first commit

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,5 +1,11 @@
+import { Message as MessageSemanticUI } from 'semantic-ui-react';
+
 function Message() {
-    return <div>Message</div>;
+    return (
+      <MessageSemanticUI>
+      La recherche a retourné XXXXX résultats
+      </MessageSemanticUI>
+      );
 }
   
 export default Message;
